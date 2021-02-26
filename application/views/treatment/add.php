259bc83443287dcbe12b2a30fb6da21488ae84f2
rahmatmatt <43551805+rahmatmatt@users.noreply.github.com>
2021-02-26 15:28:23 +0700

    add tgl_input

--- application/views/treatment/add.php
+++ application/views/treatment/add.php
@@ -4,136 +4,147 @@
 
           <div class="box box-primary">
             <div class="box-header with-border">
-                <h3 class="box-title">Add Treatment</h3>
+                <h3 class="box-title">Add Trial</h3>
             </div>
             <!-- /.box-header -->
             <!-- form start -->
             <?php
-                echo form_open_multipart('treatment/filter', 'role="form" class="form-horizontal"');
+                echo form_open_multipart('trials/add', 'role="form" class="form-horizontal"');
             ?>
 
                 <div class="box-body">
-				
-				 <div class="form-group">
+
+                  <div class="form-group">
                       <label class="col-sm-2 control-label">Trial Code</label>
 
-                      <div class="col-sm-3">
-                        <?php
-                          echo cmb_dinamis('trialcode', 'trials', 'trial_code'  , 'trial_code');				  
-                        ?>
+                      <div class="col-sm-2">
+                        <input type="text" name="trial_code" class="form-control" placeholder="Trial Code" required>
+                      </div>
+                  </div>
+
+                 
+				  
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Title</label>
+
+                      <div class="col-sm-6">
+                        <input type="text" name="title" class="form-control" placeholder="Title" required>
                       </div>
                   </div>
 				  
-				<div class="form-group">
-                      <label class="col-sm-2 control-label">Select Treatment</label>
-					  <div class="col-sm-2">
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Location</label>
 
-						<select class="form-control treatment" name="streatment">
-							<option value="">All</option>
-							<option value="Control">Control</option>
-							<option value="EFB">EFB</option>
-						</select>
-					</div>
-				</div>
-				 
-				<div class="form-group">
-                      <label class="col-sm-2 control-label">Select Time Squence</label>
-					  <div class="col-sm-2">
-
-						<select class="form-control treatment" name="">
-							<option>All</option>
-							<option value="Control">Control</option>
-							<option value="EFB">EFB</option>
-						</select>
-					</div>
-				</div>
-				
-				<div class="form-group">
-                      <label class="col-sm-2 control-label">Select Replicate</label>
-					  <div class="col-sm-2">
-
-						<select class="form-control treatment" name="">
-							<option>All</option>
-							<option value="Control">Control</option>
-							<option value="EFB">EFB</option>
-						</select>
-					</div>
-				</div>
-				
-				<div class="form-group">
-                      <label class="col-sm-2 control-label">Select Habitat Type</label>
-					  <div class="col-sm-2">
-
-						<select class="form-control treatment" name="">
-							<option>All</option>
-							<option value="Control">Control</option>
-							<option value="EFB">EFB</option>
-						</select>
-					</div>
-				</div>
-				
-				<div class="form-group">
-                        <label for="LastName" class="col-sm-2 control-label"></label>
-                        <div class="col-sm-4">
-                            <button type="submit" id="btn-filter" class="btn btn-primary">Filter</button>
-                            <button type="button" id="btn-reset" class="btn btn-default">Reset</button>
-                        </div>
-                    </div>
-         
-            
-            </div>
-            <!-- /.box-header -->
-            <div class="box-body">
-					   <table id="exampl" class="table table-striped table-bordered table-hover table-full-width dataTable" cellspacing="0" width="100%">
-                <thead>
-                    <tr>
-                        <th width="3%">No</th>
-						 <th width="24%">Trial Code</th>
-                        <th width="12%">Treatment</th>
-                        <th width="12%">Time Squence</th>
-                        <th width="11%">Replicate</th>
-                        <th width="11%">Habitat Type</th>
-						<th width="12%">Baits</th>
-						<th width="3%">Hole</th>
-						<th width="12%">Score</th>
-                       
-                    </tr>
-                </thead>
-				
-				<tbody>
-                  <?php
-                  $no = 0;
-                  foreach ($treatment->result_array() as $t) :
-                    $no++;
-                    
-                    ?>
-                    <tr>
-                      <td><?php echo $no; ?></td>
-					  <td><?php echo $t['trial_code']; ?></td>
-                      <td><?php echo $t['treatment']; ?></td>
-					  <td><?php echo $t['time_squence']; ?></td>
-					  <td><?php echo $t['replicate']; ?></td>
-					  <td><?php echo $t['habitat_type']; ?>
-					  <td><?php echo $t['baits']; ?>
-					  <td><?php echo $t['hole']; ?>
-					  <td><i style="color: white;"><?=$t['score']?></i><input type="number"  name="score" id="<?=$t['id']?>" onchange="saveData(<?=$t['id']?>)" value="<?=$t['score']?>"   min="0" max="1" required></td>				   					 
-				  </tr>
+                      <div class="col-sm-2">
+                        <input type="text" name="location" class="form-control" placeholder="location" required>
+                      </div>
+                  </div>
 				  
-                  <?php endforeach; ?>
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Block</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="block" class="form-control" placeholder="Block" required>
+                      </div>
+                  </div>
 				  
-                </tbody>
-				
-			                  
-              </table>
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Installation Date</label>
 
-                       <div class="col-sm-12" align="right">
-                        <button type="submit" name="submit" class="btn btn-success">  Save  </button>
-                      </div>      
+                      <div class="col-sm-2">
+                        <input type="date" name="installation_date" class="form-control" placeholder="Installation Date" required>
+                      </div>
+                  </div>
+				  
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Palm Age</label>
 
+                      <div class="col-sm-2">
+                        <input type="text" name="palm_age" class="form-control" placeholder="Palm Age" required>
+                      </div>
+                  </div>
+				  
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Start</label>
 
-            </div>
-            <!-- /.box-body -->
-          </div>
+                      <div class="col-sm-2">
+                        <input type="date" name="start" class="form-control" placeholder="Start" required>
+                      </div>
+                  </div>
+				  
+				  
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Finish</label>
+
+                      <div class="col-sm-2">
+                        <input type="date" name="finish" class="form-control" placeholder="Finish" required>
+                      </div>
+                  </div>
+				  
+				   <div class="form-group">
+                      <label class="col-sm-2 control-label">Treatment</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="tot_treatment" class="form-control" placeholder="Treatment" required>
+                      </div>
+                  </div>
+				  
+				  <div class="form-group">
+                      <label class="col-sm-2 control-label">Time Squence</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="tot_time_squence" class="form-control" placeholder="Time Squence" required>
+                      </div>
+                  </div>
+				  
+				  <div class="form-group">
+                      <label class="col-sm-2 control-label">Replicate</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="tot_replicate" class="form-control" placeholder="Replicate" required>
+                      </div>
+                  </div>
+				  
+				  <div class="form-group">
+                      <label class="col-sm-2 control-label">Habitat Type</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="tot_habitat_type" class="form-control" placeholder="Habitat Type" required>
+                      </div>
+                  </div>
+				  
+				  <div class="form-group">
+                      <label class="col-sm-2 control-label">Baits</label>
+
+                      <div class="col-sm-2">
+                        <input type="text" name="tot_baits" class="form-control" placeholder="Baits" required>
+                      </div>
+                  </div>
+				  
+				  <div class="form-group">
+                       <label for="quantity">Quantity (between 1 and 5):</label>
+<input type="number" id="quantity" name="quantity" min="1" max="5"> 
+                      </div>
+                  </div>
+				  
+				   <input type="hidden" name="last_update" value="<?php echo date("Y-m-d"); ?>" />
+
+                  
+
+                 
+
+                  <div class="form-group">
+                      <label class="col-sm-2 control-label"></label>
+
+                      <div class="col-sm-1">
+                        <button type="submit" name="submit" class="btn btn-success">Save</button>
+                      </div>
+
+                      
+                        <?php
+                          echo anchor('trials', 'Back', array('class'=>'btn btn-success'));
+                        ?>
+                      </div>
                   </div>
 
                 </div>
@@ -146,53 +157,3 @@
     </div>
     <!-- /.row -->
 </section>
-
-<!-- Modal -->
-<div class="modal fade" id="modal-alert" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-	<div class="modal-dialog modal-sm" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h5 class="modal-title">Peringatan</h5>
-			</div>
-			<div class="modal-body">
-			Maaf Score yang di izinkan cuma 1 dan 0 !!!
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
-			</div>
-		</div>
-	</div>
-</div>
-<script src="<?php echo base_url(); ?>assets/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?php echo base_url(); ?>assets/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
- <script type="text/javascript">
-	$(document).ready(function() {
-	    $('#exampl').DataTable();
-	    
-		$('.treatment').on('change', function () {
-	        console.log($('.treatment').val());
-	    });
-	});
-  let url="<?=base_url()?>";
-  function saveData(id) {
-    const score=$("#"+id).val();
-    const data={
-      id:id,
-      score:score
-    }
-   if (score>1) {
-		 $("#modal-alert").modal("show");
-	 } else{
-		$.ajax({
-      type: "POST",
-      url: url+"treatment/saveScore",
-      data: data,
-      dataType: "JSON",
-      success: function (response) {
-        console.log(response);
-      }
-    });
-	 }
-    }
-</script>
