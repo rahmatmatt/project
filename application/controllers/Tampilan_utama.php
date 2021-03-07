dece5271484a12e2528af6c31133f196c1147e06
rahmatmatt <43551805+rahmatmatt@users.noreply.github.com>
2021-03-07 20:39:20 +0700

    fix dashboard chart

--- application/controllers/Tampilan_utama.php
+++ application/controllers/Tampilan_utama.php
@@ -3,19 +3,29 @@
 	class Tampilan_utama extends CI_Controller
 	{
 		
+		public function __construct()
+		{
+		parent::__construct();
+		$this->load->model('model_tampilanutama');
+		}
+	
 		function index()
 		{
 			$quser = 'SELECT COUNT(*) AS hasil FROM trials';
 			$data['trials'] = $this->db->query($quser)->row_array();
 
-			$qsiswa = 'SELECT COUNT(*) AS hasil FROM trials';
-			$data['trials'] = $this->db->query($qsiswa)->row_array();
+			$tgl_sekarang=date("Y-m-d");
+			$qsiswa = 'SELECT COUNT(*) AS hasil FROM trials where finish >= date(now())  ';
+			$data['aktif'] = $this->db->query($qsiswa)->row_array();
 
 			$qguru = 'SELECT COUNT(*) AS hasil FROM trials';
 			$data['trials'] = $this->db->query($qguru)->row_array();
 
 			$qruangan = 'SELECT COUNT(*) AS hasil FROM trials';
 			$data['trials'] = $this->db->query($qruangan)->row_array();
+			
+			
+			$data['graph'] = $this->model_tampilanutama->graph();
 
 			$this->template->load('template', 'dashboard', $data);
 		}
